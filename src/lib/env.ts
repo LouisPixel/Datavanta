45ac55538edca4f4a4bcf065bfa267ca985a14b9
Louis Dang <louisdang495@gmail.com>
2026-01-05 04:32:26 -0600

    fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,7 +2,6 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
  
 export const env = createEnv({
-
   server: {
     DATABASE_URL: z.string().url(),
     BETTER_AUTH_SECRET: z.string().min(1),
@@ -10,7 +9,11 @@ export const env = createEnv({
     AUTH_GOOGLE_CLIENT_ID: z.string().min(1),
     AUTH_GOOGLE_SECRET: z.string().min(1),
   },
-
-
- experimental__runtimeEnv: {},
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    AUTH_GOOGLE_CLIENT_ID: process.env.AUTH_GOOGLE_CLIENT_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+  },
 });
