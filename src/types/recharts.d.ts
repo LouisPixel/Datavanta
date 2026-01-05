8872e9e029564d58f2762b26d996d5288b5ba035
Louis Dang <louisdang495@gmail.com>
2026-01-05 04:20:51 -0600

    fix 3

--- src/types/recharts.d.ts
+++ src/types/recharts.d.ts
@@ -4,17 +4,27 @@
 import 'recharts';
 import { ReactElement } from 'react';
 
+interface LabelProps {
+  x?: number;
+  y?: number;
+  payload?: {
+    value?: number | string;
+    [key: string]: unknown;
+  };
+  [key: string]: unknown;
+}
+
 declare module 'recharts' {
   // Extend Line component to accept stackId and custom label
   interface LineProps {
     stackId?: string;
-    label?: boolean | ReactElement | ((props: any) => ReactElement);
+    label?: boolean | ReactElement | ((props: LabelProps) => ReactElement);
   }
 
   // Extend Area component to accept stackId and custom label
   interface AreaProps {
     stackId?: string;
-    label?: boolean | ReactElement | ((props: any) => ReactElement);
+    label?: boolean | ReactElement | ((props: LabelProps) => ReactElement);
   }
 
   // Extend Bar component to accept stackId
